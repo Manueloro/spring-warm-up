9df20f6e866b7cb00a994df9f1e363f47be31de4
Manueloro <oro.manuel@hotmail.com>
2025-02-20 11:51:50 +0100

    refactor: better controller structure and exceptions

--- src/main/java/ch/etmles/payroll/Controllers/Exceptions/RessourceNotFoundAdvice.java
+++ src/main/java/ch/etmles/payroll/Controllers/Exceptions/RessourceNotFoundAdvice.java
@@ -1,4 +1,4 @@
-package ch.etmles.payroll.Controllers.Employee;
+package ch.etmles.payroll.Controllers.Exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -6,20 +6,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-class RessourceIDNotFound extends RuntimeException {
-    public RessourceIDNotFound(Long id, String ressourceName) {
-        super("Could not find " + ressourceName + " " + id);
-    }
-}
-
-class RessourceDeleteNotFound extends RuntimeException {
-    public RessourceDeleteNotFound(Long id, String ressourceName) {
-        super("Could not delete " + ressourceName + " (not found): " + id);
-    }
-}
-
 @ControllerAdvice
-class RessourceNotFoundAdvice {
+public class RessourceNotFoundAdvice {
 
     @ResponseBody
     @ExceptionHandler(RessourceIDNotFound.class)
